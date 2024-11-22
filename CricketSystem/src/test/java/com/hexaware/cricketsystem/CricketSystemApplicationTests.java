6cb76ebfea16a82132593204e762e6930d110351
vinayak soni <sonivinayak716@gmail.com>
2024-11-22 12:27:18 +0530

    Added codeCahllenge

---
+++ CricketSystem/src/test/java/com/hexaware/cricketsystem/CricketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.cricketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
